00b4aedbca70a9ef66e73e770cc623ef624f9d3f
lalthanpuiafanai <lalthanpuiafanai@10.180.208.56>
2019-07-05 10:59:45 +0530

    2nd commit

--- msegs/src/main/java/com/thanpuia/oath2/msegs/MsegsApplication.java
+++ msegs/src/main/java/com/thanpuia/oath2/msegs/MsegsApplication.java
@@ -1,10 +1,46 @@
 package com.thanpuia.oath2.msegs;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.thanpuia.oath2.msegs.entity.Role;
+import com.thanpuia.oath2.msegs.entity.User;
+import com.thanpuia.oath2.msegs.services.UserService;
+
+
 
 @SpringBootApplication
 public class MsegsApplication {
+	
+	
+	@Bean
+    public CommandLineRunner setupDefaultUser(UserService service) {
+        return args -> {
+        	
+        
+            service.save(new User(1, //id
+                   "user1", //username
+                    "user", //password
+                    "thanpuia@gmail.com",
+                    "9158086237",
+            	
+            		
+Arrays.asList(new Role(1,"ADMIN")),//roles 
+                    true//Active
+            ));
+        };
+    }   
+    
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }    
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsegsApplication.class, args);
